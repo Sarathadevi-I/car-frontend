ebf34c5944c2a5e0cfaa9b134d4dba76f05b7c98
Radnus Developer <webdevelopmentdept@gmail.com>
2026-08-04 12:11:39 +0530

    Change backend port4

--- src/Components/Pages/Vehiclegroupselect.jsx
+++ src/Components/Pages/Vehiclegroupselect.jsx
@@ -259,7 +259,7 @@ export default function VehicleGroupSelect() {
             </AnimatePresence>
           </motion.div>
         )}
-{/* code */}
+
         <AnimatePresence>
           {!loading && filtered.length === 0 && (
             <motion.p
